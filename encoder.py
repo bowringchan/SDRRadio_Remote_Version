0c9c1e3a5114a32dc30aa51a528b4203c04ce262
BowringChan <bowringchan@gmail.com>
2017-01-29 06:32:15 -0800

    Changes to be committed: 	modified:   current_progress.txt 	modified:   dsp.py 	modified:   encoder.py 	modified:   fifo_tool.py 	modified:   m3u8_generator.py  A main.py program is needed

--- encoder.py
+++ encoder.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import subprocess
-
+import m3u8_generator
 
 class batch_encoder:
     def __init__(self,fifo_desc):
         self.fifo_desc = fifo_desc
         self.output_list = []
         self.output_counter = 1
+        self.EXTINF = 1
+        self.media_sequence = 1
 
     def encode(self):
         # 1s 8bit linear PCM raw audio data take 192kb = 24kB
-        buf = self.fifo_desc.read(24000)
+        buf = self.fifo_desc.read(24000*self.EXTINF)
         output = open(r'audio\audio'+self.output_counter+r'.raw','w')
         output.write(buf)
         output.close()
@@ -20,3 +22,13 @@ class batch_encoder:
         #lame -r -s 48 --bitwidth 8 --unsigned --quiet
         subprocess.call(["lame","-r","-s","48","--bitwidth 8","--unsigned",r'audio\audio'+self.output_counter+r'.raw',r'audio\audio'+self.output_counter+r'.mp3'])
         self.output_list.append(r'audio\audio'+self.output_counter+r'.mp3')
+        if self.output_list.__len__() > 3:
+            self.output_list.pop(0)
+
+    #ENTRY
+    def encode_mkm3u8(self):
+        self.encode()
+        m3u8_generator_i = m3u8_generator(self.EXTINF)
+        if self.output_counter > 2:
+            m3u8_generator_i.generate(self, self.output_list, self.media_sequence)
+            self.media_sequence += 1
--- m3u8_generator.py
+++ m3u8_generator.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+from threading import Timer
 
 class m3u8_generator:
-    def generate(self,output_list,EXTINF,media_sequence):
+    def __init__(self,EXTINF):
+        self.EXTINF = EXTINF
+
+    def generate(self,output_list,media_sequence):
         m3u8file = open(r'audio\RTLSDR.m3u','w')
-        m3u8file.write(r'#EXTM3U\n'+r'#EXT-X-TARGETDURATION:10\n'\
+        m3u8file.write(r'#EXTM3U\n'+r'#EXT-X-TARGETDURATION:1\n'\
                        r'#EXT-X-VERSION:3'+r'#EXT-X-MEDIA-SEQUENCE:'+media_sequence+r'\n')
         for list_item in output_list:
-            m3u8file.write('#EXTINF:'+EXTINF+','+r'\n'+list_item+r'\n')
+            m3u8file.write('#EXTINF:'+self.EXTINF+','+r'\n'+list_item+r'\n')
         m3u8file.close()
